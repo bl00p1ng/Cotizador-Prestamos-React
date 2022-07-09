4ce1bdee9000689005d4646e35aeb4ab46b8916c
bl00p1ng <blooping@protonmail.com>
2022-07-09 16:43:30 -0500

    351 Comenzando nuestro primer proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import React, { Fragment } from 'react';
+
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
