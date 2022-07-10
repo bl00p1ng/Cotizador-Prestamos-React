c8c94c4a4f81957e163cb9fa88dc327f75569cb0
bl00p1ng <blooping@protonmail.com>
2022-07-10 08:03:32 -0500

    358 Mostrar mensaje de error

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, Fragment} from 'react'
 
 const Form = ({quantity, saveQuantity, period, savePeriod}) => {
   // Crear state para un mensaje de error
@@ -11,43 +11,52 @@ const Form = ({quantity, saveQuantity, period, savePeriod}) => {
       // Validar form
       if (quantity === 0 || period === '') {
         saveError(true)
+        return;
       }
+
+      // Eliminar error previo
+      saveError(false)
   }
 
   return (
-    <form onSubmit={ calculateLoan }>
-      <div className="row">
-        <div>
-          <label>Cantidad Prestamo</label>
-          <input 
-            className="u-full-width" 
-            type="number" 
-            placeholder="Ejemplo: 3000" 
-            onChange={ e => saveQuantity(parseInt(e.target.value)) }
-          />
-        </div>
-        <div>
-          <label>Plazo para Pagar</label>
-          <select 
-            className="u-full-width"
-            onChange={ e => savePeriod(parseInt(e.target.value)) }
-          >
-            <option value="">Seleccionar</option>
-            <option value="3">3 meses</option>
-            <option value="6">6 meses</option>
-            <option value="12">12 meses</option>
-            <option value="24">24 meses</option>
-          </select>
-        </div>
+    <Fragment>
+      <form onSubmit={ calculateLoan }>
+        <div className="row">
           <div>
+            <label>Cantidad Prestamo</label>
             <input 
-              type="submit" 
-              value="Calcular" 
-              className="button-primary u-full-width" 
+              className="u-full-width" 
+              type="number" 
+              placeholder="Ejemplo: 3000" 
+              onChange={ e => saveQuantity(parseInt(e.target.value)) }
             />
           </div>
-      </div>
-    </form>
+          <div>
+            <label>Plazo para Pagar</label>
+            <select 
+              className="u-full-width"
+              onChange={ e => savePeriod(parseInt(e.target.value)) }
+            >
+              <option value="">Seleccionar</option>
+              <option value="3">3 meses</option>
+              <option value="6">6 meses</option>
+              <option value="12">12 meses</option>
+              <option value="24">24 meses</option>
+            </select>
+          </div>
+            <div>
+              <input 
+                type="submit" 
+                value="Calcular" 
+                className="button-primary u-full-width" 
+              />
+            </div>
+        </div>
+      </form>
+
+      {/* Validar si se muestra o no el error */}
+      {(error) ? <p className="error">Todos los campos son obligatorios</p> : null}
+    </Fragment>
   )
 }
 
